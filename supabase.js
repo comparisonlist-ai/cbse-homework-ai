1b168b3289c127555eee1dad8bfb40571285648f
comparisonlist-ai <comparisonlist@gmail.com>
2026-07-29 16:16:48 +0530

    Initialize Supabase client with URL and key

--- supabase.js
+++ supabase.js
@@ -1,1 +1,14 @@
 
+
+const SUPABASE_URL = "https://gqzrnfjriftgti lxaeyn.supabase.co
+";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_wI54vtIIhCmCNqSB2TzB0Q_9oezFv3Q
+";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
+
+
+
